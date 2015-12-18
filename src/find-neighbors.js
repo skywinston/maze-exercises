4770cae94f435d477b6c8b55f99790d51f2c6e9e
Sky Winston <skywinston@gmail.com>
2015-12-18 11:04:44 -0700

    started work on findNeighbors function

--- src/find-neighbors.js
+++ src/find-neighbors.js
@@ -3,7 +3,7 @@
 //
 //  - return the points in the order of up-right-down-left (clockwise from the node)
 //  - don't include any points that would be out-of-bounds
-//  - don't inlcude points whose value is the "-" character
+//  - don't include points whose value is the "-" character
 //  - don't include any points whose y/x values match the y/x values of the _exclude_ parameter
 //
 // WHY? - this is the function you'll use in your maze solution to determine the "children"
@@ -14,5 +14,16 @@ module.exports = function findNeighbors(maze, point, exclude) {
     [y, x + 1], // right (east)
     [y + 1, x], // down (south)
     [y, x - 1]  // left (west)
-  ]
-}
+  ];
+  var y = point[0];
+  var x = point[1];
+  var neighbors = [];
+
+  if (maze[y - 1][x] !== '-' && maze[y - 1][x] !== undefined) neighbors.push(directions[0]);
+  if (maze[y][x + 1] !== '-' && maze[y][x + 1] !== undefined) neighbors.push(directions[1]);
+  if (maze[y + 1][x] !== '-' && maze[y + 1][x] !== undefined) neighbors.push(directions[2]);
+  if (maze[y][x - 1] !== '-' && maze[y][x - 1] !== undefined) neighbors.push(directions[3]);
+
+  return neighbors;
+
+};
